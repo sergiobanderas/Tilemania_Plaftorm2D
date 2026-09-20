3c6973ff6fffdd2de0638e3e5ff4b10c60a7b1a3
sergiobanderas <sergio.banderas@fpalanturing.es>
2026-09-20 19:02:28 +0200

    Horizontal Move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,28 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float runSpeed = 5f;    
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();   
     }
 
     void OnMove(InputValue value)
     {
-        moveInput = value.Get<Vector2>();
-        Debug.Log("Move Input: " + moveInput);
+        moveInput = value.Get<Vector2>();        
+    }
+
+    void Run()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * runSpeed, rb.linearVelocity.y);
     }
 }
